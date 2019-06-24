7ac2c09e7a7a4c1b9870bca3e97c2e2809549f2a
Michał Wilkowski <m.wilkowski84@wp.pl>
2019-06-24 19:39:56 +0200

    Add js/functions

--- js/script.js
+++ js/script.js
@@ -1,9 +1,3 @@
-function printMessage(msg){
-	var div = document.createElement('div');
-	div.innerHTML = msg;
-	document.getElementById('messages').appendChild(div);
-}
-
-function clearMessages(){
-	document.getElementById('messages').innerHTML = '';
-}
+var playerMove;
+playerMove = 'papier';
+printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nożyczki, to wygrywasz!');
