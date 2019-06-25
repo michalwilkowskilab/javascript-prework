ed7a4c1007cb8d5a989bf19e2e0cddcc6c2be2e1
Michał Wilkowski <m.wilkowski84@wp.pl>
2019-06-25 20:15:06 +0200

    Add player's answers/js.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,3 @@
-var playerMove;
-playerMove = 'papier';
-printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nożyczki, to wygrywasz!');
-
 var computerMove, randomNumber;
 randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log('wylosowana liczba to: ' + randomNumber);
@@ -15,3 +11,20 @@ if (randomNumber == '1') {
   computerMove = 'nieznany ruch';
 }
 printMessage('Mój ruch: ' + computerMove);
+
+var playerInput, playerMove;
+playerInput = 0;
+
+playerMove;
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput == '1') {
+  playerMove = 'kamień';
+} else if (playerInput == '2') {
+  playerMove = 'papier';
+} else if (playerInput == '3') {
+  playerMove = 'nożyce';
+} else {
+  playerMove = 'nieznany ruch';
+}
+printMessage('Twój ruch: ' + playerMove);
